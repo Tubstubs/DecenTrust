0ec6932c18ae2d09bbad864d3417eac9aedaf8e4
Zer01427 <114202412+Tubstubs@users.noreply.github.com>
2023-04-07 10:06:25 +0800

    change navbar

--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -1,46 +1,75 @@
-import {
-    Link
-} from "react-router-dom";
-import { Navbar, Nav, Button, Container } from 'react-bootstrap'
-import market from './market.png'
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Button, Container, Form } from "react-bootstrap";
+import market from "./market.png";
 
 const Navigation = ({ web3Handler, account }) => {
     return (
         <Navbar expand="lg" bg="secondary" variant="dark">
             <Container>
                 <Navbar.Brand href="http://www.dappuniversity.com/bootcamp">
-                    <img src={market} width="40" height="40" className="" alt="" />
-                    &nbsp; DApp NFT Marketplace
+                    <img
+                        src={market}
+                        width="40"
+                        height="40"
+                        className=""
+                        alt=""
+                    />
+                    &nbsp; DecenTrust
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/create">Create</Nav.Link>
-                        <Nav.Link as={Link} to="/my-listed-items">My Listed Items</Nav.Link>
-                        <Nav.Link as={Link} to="/my-purchases">My Purchases</Nav.Link>
-                    </Nav>
+                    {/* Links to other pages */}
+                    {/* <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/create">
+              Create
+            </Nav.Link>
+            <Nav.Link as={Link} to="/my-listed-items">
+              My Listed Items
+            </Nav.Link>
+            <Nav.Link as={Link} to="/my-purchases">
+              My Purchases
+            </Nav.Link>
+          </Nav> */}
                     <Nav>
+                        <Form className="d-flex mx-4">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                            />
+                            <Button variant="outline-light" type="submit">
+                                Search
+                            </Button>
+                        </Form>
                         {account ? (
                             <Nav.Link
                                 href={`https://etherscan.io/address/${account}`}
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="button nav-button btn-sm mx-4">
+                                className="button nav-button btn-sm mx-4"
+                            >
                                 <Button variant="outline-light">
-                                    {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                                    {account.slice(0, 5) +
+                                        "..." +
+                                        account.slice(38, 42)}
                                 </Button>
-
                             </Nav.Link>
                         ) : (
-                            <Button onClick={web3Handler} variant="outline-light">Connect Wallet</Button>
+                            <Button
+                                onClick={web3Handler}
+                                variant="outline-light"
+                            >
+                                Connect Wallet
+                            </Button>
                         )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
-
-}
+    );
+};
 
 export default Navigation;
