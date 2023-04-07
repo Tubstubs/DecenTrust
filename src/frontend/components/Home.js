ad93eb098aa1afc9043b193de426ca2d1d6ea3cd
Zer01427 <114202412+Tubstubs@users.noreply.github.com>
2023-04-07 16:14:21 +0800

    fix bugs, change navbar and homepage

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -1,85 +1,112 @@
-import { useState, useEffect } from 'react'
-import { ethers } from "ethers"
-import { Row, Col, Card, Button } from 'react-bootstrap'
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { Row, Col, Card, Button, Carousel } from "react-bootstrap";
+import image1 from "./pictures/netflix.png";
+import image2 from "./pictures/disney_plus.png";
 
 const Home = ({ marketplace, nft }) => {
-  const [loading, setLoading] = useState(true)
-  const [items, setItems] = useState([])
-  const loadMarketplaceItems = async () => {
-    // Load all unsold items
-    const itemCount = await marketplace.itemCount()
-    let items = []
-    for (let i = 1; i <= itemCount; i++) {
-      const item = await marketplace.items(i)
-      if (!item.sold) {
-        // get uri url from nft contract
-        const uri = await nft.tokenURI(item.tokenId)
-        // use uri to fetch the nft metadata stored on ipfs 
-        const response = await fetch(uri)
-        const metadata = await response.json()
-        // get total price of item (item price + fee)
-        const totalPrice = await marketplace.getTotalPrice(item.itemId)
-        // Add item to items array
-        items.push({
-          totalPrice,
-          itemId: item.itemId,
-          seller: item.seller,
-          name: metadata.name,
-          description: metadata.description,
-          image: metadata.image
-        })
-      }
+    const [loading, setLoading] = useState(true);
+    const [items, setItems] = useState([]);
+    const loadMarketplaceItems = async () => {
+        // Load all unsold items
+        const itemCount = await marketplace.itemCount();
+        let items = [];
+        for (let i = 1; i <= itemCount; i++) {
+            const item = await marketplace.items(i);
+            if (!item.sold) {
+                // get uri url from nft contract
+                const uri = await nft.tokenURI(item.tokenId);
+                // use uri to fetch the nft metadata stored on ipfs
+                const response = await fetch(uri);
+                const metadata = await response.json();
+                // get total price of item (item price + fee)
+                const totalPrice = await marketplace.getTotalPrice(item.itemId);
+                // Add item to items array
+                items.push({
+                    totalPrice,
+                    itemId: item.itemId,
+                    seller: item.seller,
+                    name: metadata.name,
+                    description: metadata.description,
+                    image: metadata.image,
+                });
+            }
+        }
+        setLoading(false);
+        setItems(items);
+    };
+
+    const buyMarketItem = async (item) => {
+        await (
+            await marketplace.purchaseItem(item.itemId, {
+                value: item.totalPrice,
+            })
+        ).wait();
+        loadMarketplaceItems();
+    };
+
+    useEffect(() => {
+        loadMarketplaceItems();
+    }, []);
+    if (loading) {
+        return (
+            <main style={{ padding: "1rem 0" }}>
+                <h2>Loading...</h2>
+            </main>
+        );
     }
-    setLoading(false)
-    setItems(items)
-  }
-
-  const buyMarketItem = async (item) => {
-    await (await marketplace.purchaseItem(item.itemId, { value: item.totalPrice })).wait()
-    loadMarketplaceItems()
-  }
-
-  useEffect(() => {
-    loadMarketplaceItems()
-  }, [])
-  if (loading) return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Loading...</h2>
-    </main>
-  )
-  return (
-    <div className="flex justify-center">
-      {items.length > 0 ?
-        <div className="px-5 container">
-          <Row xs={1} md={2} lg={4} className="g-4 py-5">
-            {items.map((item, idx) => (
-              <Col key={idx} className="overflow-hidden">
-                <Card>
-                  <Card.Img variant="top" src={item.image} />
-                  <Card.Body color="secondary">
-                    <Card.Title>{item.name}</Card.Title>
-                    <Card.Text>
-                      {item.description}
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Footer>
-                    <div className='d-grid'>
-                      <Button onClick={() => buyMarketItem(item)} variant="primary" size="lg">
-                        Buy for {ethers.utils.formatEther(item.totalPrice)} ETH
-                      </Button>
-                    </div>
-                  </Card.Footer>
-                </Card>
-              </Col>
-            ))}
-          </Row>
+    return (
+        <div className="flex justify-center">
+            {items.length > 0 ? (
+                <div className="px-5 container">
+                    <Row xs={1} md={2} lg={4} className="g-4 py-5">
+                        {items.map((item, idx) => (
+                            <Col key={idx} className="overflow-hidden">
+                                <Card>
+                                    <Card.Img variant="top" src={item.image} />
+                                    <Card.Body color="secondary">
+                                        <Card.Title>{item.name}</Card.Title>
+                                        <Card.Text>
+                                            {item.description}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <div className="d-grid">
+                                            <Button
+                                                onClick={() =>
+                                                    buyMarketItem(item)
+                                                }
+                                                variant="primary"
+                                                size="lg"
+                                            >
+                                                Buy for{" "}
+                                                {ethers.utils.formatEther(
+                                                    item.totalPrice
+                                                )}{" "}
+                                                ETH
+                                            </Button>
+                                        </div>
+                                    </Card.Footer>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
+            ) : (
+                <main style={{ padding: "1rem 0" }}>
+                    <h2>No listed assets</h2>
+                </main>
+            )}
+            <Carousel>
+                <Carousel.Item>
+                    <img src={image1} alt="Netflix 1" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={image2} alt="Netflix 2" />
+                </Carousel.Item>
+            </Carousel>
         </div>
-        : (
-          <main style={{ padding: "1rem 0" }}>
-            <h2>No listed assets</h2>
-          </main>
-        )}
-    </div>
-  );
-}
-export default Home
+    );
+};
+
+export default Home;
